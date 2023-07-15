2c55401e8beb39d713cc9005b494f07be0532f68
Jayden Richardson <jayrich.dev@gmail.com>
2023-07-15 17:23:37 +0100

    dates

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,7 +2,30 @@ import styled from "styled-components";
 import React from "react";
 
 function Footer() {
-  return <FooterContainer>{new Date().getFullYear()}</FooterContainer>;
+  const date = new Date();
+
+  const dayName = date.toLocaleString("default", { weekday: "long" });
+  const monthName = date.toLocaleString("default", { month: "long" });
+
+  const formattedDate = `${dayName}, ${date.getDate()} ${monthName} ${date.getFullYear()}`;
+  const formattedTime = date.toLocaleTimeString();
+
+  return (
+    <FooterContainer
+      style={{
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+        textAlign: "center",
+        backgroundColor: "#f8f9fa",
+        padding: "10px 0",
+        borderTop: "1px solid #dee2e6",
+      }}
+    >
+      <p style={{ margin: "0", color: "#6c757d" }}>{formattedDate}</p>
+      <p style={{ margin: "0", color: "#6c757d" }}>{formattedTime}</p>
+    </FooterContainer>
+  );
 }
 
 const FooterContainer = styled.footer`
